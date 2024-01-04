83c00e6b760f9eb6a38c69756a75f4d7119e894b
Choi-Hyeok99 <gur0709@naver.com>
2024-01-04 09:23:22 +0900

    StringBuilder 다른 방법

--- src/main/java/CT2024/january/StringBuilder_20240104.java
+++ src/main/java/CT2024/january/StringBuilder_20240104.java
@@ -6,10 +6,21 @@ class StringBuilder_20240104 {
     public ArrayList<String> solution(int n, String[] str) {
         ArrayList<String> answer= new ArrayList<>();
 
-        for(String x: str){
-            String tmp = new StringBuilder(x).reverse().toString();
+        for(String x : str ){
+            char[] s = x.toCharArray();
+            int lt = 0 , rt = x.length()-1;
+            while (lt<rt){
+                char tmp = s[lt];
+                s[lt]=s[rt];
+                s[rt]=tmp;
+                lt++;
+                rt--;
+            }
+            String tmp =String.valueOf(s);
             answer.add(tmp);
+
         }
+
         return answer;
     }
     public static void main(String[] args) {
