3750436fd68a01b5dfcb1304cd316b5edf8c6f3c
Choi-Hyeok99 <gur0709@naver.com>
2024-01-05 12:33:40 +0900

    숫자만 추출 + 내용추가

--- src/main/java/CT2024/january/inplearn_9.java
+++ src/main/java/CT2024/january/inplearn_9.java
@@ -11,7 +11,7 @@ class inplearn_9 {
     public int solution(String s){
         int answer = 0;
         for( char x : s.toCharArray()){
-            if ((x>=48 && x<=57)) answer = answer * 10 +(x-48);
+            if ((x>=48 && x<=57)) answer = answer * 10 +(x-48); // ASCII 48~58은 숫자 0~9까지이다.
         }
     return answer;
 }
