4ba9a3fe824ef3c18a442aa60b8cc69a4533e572
Choi-Hyeok99 <gur0709@naver.com>
2024-01-30 09:04:41 +0900

    백준 사칙연산 + ??!

---
+++ src/main/java/CT2401/Baekjoon/p10926.java
@@ -0,0 +1,12 @@
+package CT2401.Baekjoon;
+
+import java.util.Scanner;
+
+public class p10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str + "??!");
+
+    }
+}
