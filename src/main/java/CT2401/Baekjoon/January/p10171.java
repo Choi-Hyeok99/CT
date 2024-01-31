b103b9324ccabe1b9b947367ac5060ce8eb11568
Choi-Hyeok99 <gur0709@naver.com>
2024-01-31 15:09:28 +0900

    백준 고양이 ( 단순 출력 )

---
+++ src/main/java/CT2401/Baekjoon/January/p10171.java
@@ -0,0 +1,23 @@
+package CT2401.Baekjoon.January;
+
+/*
+문제
+아래 예제와 같이 고양이를 출력하시오.
+
+입력
+없음.
+
+출력
+고양이를 출력한다.
+
+
+ */
+public class p10171 {
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
