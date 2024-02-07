d9306beb94f8c8ec195c7145acb439a58975bff6
Choi-Hyeok99 <gur0709@naver.com>
2024-02-07 17:41:54 +0900

    백준 :A+B-7(반복문 )

---
+++ src/main/java/CT/Baekjoon/February/p11021.java
@@ -0,0 +1,19 @@
+package CT.Baekjoon.February;
+
+import java.util.Scanner;
+
+public class p11021 {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        for (int i = 1; i <= a; i++) {
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+
+            System.out.println("Case #" + i + ": " + (c + d));
+        }
+    }
+}
