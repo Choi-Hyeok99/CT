f36281056b84189a7d5d82c5454ddaece4d9653b
Choi-Hyeok99 <gur0709@naver.com>
2024-02-21 16:02:27 +0900

    백준 : 문자와 문자열 ( 문자열 )

---
+++ src/main/java/CT/Baekjoon/String/p27866.java
@@ -0,0 +1,16 @@
+package CT.Baekjoon.String;
+
+
+import java.util.Scanner;
+
+public class p27866 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        int i = sc.nextInt()-1;
+
+        System.out.println(s.charAt(i));
+    }
+}
