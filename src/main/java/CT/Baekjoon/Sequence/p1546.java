9c8f0201df39354934abc88f41c3fcdade255b0a
Choi-Hyeok99 <gur0709@naver.com>
2024-03-07 15:34:29 +0900

    백준 : 평균 ( 1차원 배열 )

---
+++ src/main/java/CT/Baekjoon/Sequence/p1546.java
@@ -0,0 +1,24 @@
+package CT.Baekjoon.Sequence;
+
+import java.util.Scanner;
+
+public class p1546 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int M = sc.nextInt();
+        double sum = 0;
+        double max = 0;
+
+        for ( int i = 0 ; i < M ; i++) {
+            int num = sc.nextInt();
+            if (num > max) {
+                max = num;
+            }
+            sum += num;
+        }
+        sc.close();
+        System.out.println(((sum/max)*100)/M);
+    }
+}
