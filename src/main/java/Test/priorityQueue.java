5b0e4883cf01cbbb62960e5301c057373afede17
Choi-Hyeok99 <gur0709@naver.com>
2024-03-13 13:49:41 +0900

    정렬 전략 설정 예제 1

--- src/main/java/Test/priorityQueue.java
+++ src/main/java/Test/priorityQueue.java
@@ -3,6 +3,8 @@ package Test;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
+
+/* 1. 기본예제
 public class priorityQueue {
     public static void main(String[] args) {
 
@@ -17,7 +19,47 @@ public class priorityQueue {
         System.out.print("출력 : ");
         for(int i = 0 ; i<10; i++){
             System.out.println(pq.poll());
+            }
         }
+    }
+ */
+ // 2. 정렬 전략 설정 예제 1
+ // 숫자 10개를 입력받아 오름차순으로 정렬하되, 홀수가 짝수보다 우선순위가 높다고 가정
+public class priorityQueue{
+    public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        PriorityQueue pq = new PriorityQueue<>((o1, o2) -> {
+          // 파라미터로 받은 o1,o2는 기본적으로 object형이며
+          // string으로 변환 후 , int형으로 다시 변환
+          int x = Integer.parseInt(o1.toString());
+          int y = Integer.parseInt(o2.toString());
+
+          // x,y가 들어왔을 때  , x에 높은 우선순위를 주고싶다면 음수값 return
+            if(x % 2 == y % 2){
+                return x-y;
+            } else {
+                if(x%2 == 1 && y % 2 ==0){
+                    return -1;
+                } else {
+                    return 1;
+                }
+            }
+        });
+
+        System.out.print("입력 : ");
+        for(int i = 0 ; i<10; i++){
+            pq.add(sc.nextInt());
+        }
+        System.out.print("출력 : ");
+        for (int i = 0 ; i<10; i++){
+            System.out.print(pq.poll()+ " ");
+
+            // Ex ) 입력 : 1 2 3 4 5 6 7 8 9 10
+            // Ex ) 출력 : 1 3 5 7 9 2 4 6 8 10
+        }
     }
 }
+
+
