bc41153a8103d5b488cf67f61c58696f23428285
Choi-Hyeok99 <gur0709@naver.com>
2024-03-18 12:48:22 +0900

    다른 버전 풀이

--- src/main/java/CT/Baekjoon/Conditional/p14681.java
+++ src/main/java/CT/Baekjoon/Conditional/p14681.java
@@ -1,5 +1,8 @@
-package CT.Baekjoon.Repetition;
+package CT.Baekjoon.Conditional;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /*
@@ -21,20 +24,23 @@ import java.util.Scanner;
 3. 출력
 점 (x, y)의 사분면 번호(1, 2, 3, 4 중 하나)를 출력한다.
  */
-public class p14681 {
-    public static void main(String[] args) {
+//
 
-        Scanner scanner = new Scanner(System.in);
-        int A = scanner.nextInt();
-        int B = scanner.nextInt();
+public class p14681{
+    public static void main(String[] args)throws IOException {
 
-        if ( A > 0 && B > 0){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        if(A>0 && B>0){
             System.out.println(1);
-        } else if ( A < 0 && B > 0 ){
+        } else if (A<0 && B>0){
             System.out.println(2);
-        } else if ( A < 0 && B < 0 ){
+        } else if (A<0 && B<0){
             System.out.println(3);
-        } else if ( A > 0 && B < 0 ){
+        } else if ( A>0 && B<0 ) {
             System.out.println(4);
         }
     }
