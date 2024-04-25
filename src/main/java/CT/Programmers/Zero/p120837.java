5452766fc9ebdc95334d9c0a5c3413ed4baf4c9a
Choi-Hyeok99 <gur0709@naver.com>
2024-04-25 09:57:43 +0900

    프로그래머스 : 개미군단

---
+++ src/main/java/CT/Programmers/Zero/p120837.java
@@ -0,0 +1,17 @@
+package CT.Programmers.Zero;
+
+class p120837 {
+    public int solution(int hp) {
+        int answer = 0;
+
+        int a = hp / 5;
+        hp = hp % 5;
+        int b = hp / 3;
+        hp = hp % 3;
+
+        answer = a + b + hp;
+
+        return answer;
+    }
+}
+
