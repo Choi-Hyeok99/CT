e356fdc861396a62459a67da93f66250c4d70222
Choi-Hyeok99 <gur0709@naver.com>
2024-06-03 14:53:26 +0900

    인프런 : 단어 뒤집기 ( 방법 1 )

---
+++ src/main/java/CT2401/inflearn/Inflearn_4/Inflearn_4.java
@@ -0,0 +1,27 @@
+package CT2401.inflearn.Inflearn_4;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Inflearn_4 {
+    public ArrayList<String> solution(int n , String[]  str){
+        ArrayList<String> answer = new ArrayList<>();
+        for(String x : str){
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Inflearn_4 T = new Inflearn_4();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String[] str = new String[n];
+        for(int i=0; i<n; i++){
+            str[i] = kb.next();
+        }
+        for(String x : T.solution(n, str)){
+            System.out.println(x);
+        }
+    }
+}
