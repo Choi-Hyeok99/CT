2d2b171f30d0968c6545780f8337b1cfb56ff49b
Choi-Hyeok99 <gur0709@naver.com>
2024-11-13 12:51:54 +0900

    백준 : 반복문

---
+++ src/main/java/Baekjoon/B/p2438.java
@@ -0,0 +1,29 @@
+package Baekjoon.B;
+
+
+import java.util.Scanner;
+
+/**
+ * 1.문제
+ * 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+ *
+ * 2.입력
+ * 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+ *
+ * 3.출력
+ * 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+ */
+public class p2438 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        for (int i=1; i<=N; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
