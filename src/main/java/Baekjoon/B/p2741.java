9467aaac43607c7ec23184b3e9683fb33d80f2f4
Choi-Hyeok99 <gur0709@naver.com>
2024-11-17 17:53:02 +0900

    백준 : N찍기   ( 백준 클래스레벨 올리기)

---
+++ src/main/java/Baekjoon/B/p2741.java
@@ -0,0 +1,19 @@
+package Baekjoon.B;
+
+import java.util.Scanner;
+
+/**
+ *
+ */
+public class p2741 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for (int i=1; i<=N; i++){
+            System.out.println(i);
+        }
+    }
+}
