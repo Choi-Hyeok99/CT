7944e62878dba5bb4be59bae5043e98057cbb295
Choi Hyeok <114526859+Choi-Hyeok99@users.noreply.github.com>
2024-11-29 11:35:21 +0900

    [Bronze I] Title: 평균, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,22 +1,34 @@
-import java.util.Scanner;
 
+import java.io.*;
+
+
+/**
+ *
+ */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
 
-        int M = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 과목의 개수
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N]; // 넣을 배열
+        // 각 과목의 시험 성적
+
+        // 각 과목의 시험 성적 입력 처리 ( 공백 구분 )
+        String[] input = br.readLine().split(" ");
+        for(int i=0; i<N; i++){
+            A[i] = Integer.parseInt(input[i]);
+        }
+
         double sum = 0;
         double max = 0;
 
-        for ( int i = 0 ; i < M ; i++) {
-            int num = sc.nextInt();
-            if (num > max) {
-                max = num;
-            }
-            sum += num;
+        for(int i=0; i<N; i++){
+            if(A[i] > max ) max = A[i];
+            sum += A[i];
         }
-        sc.close();
-        System.out.println(((sum/max)*100)/M);
+        System.out.println(sum * 100 / max / N);
     }
 }
