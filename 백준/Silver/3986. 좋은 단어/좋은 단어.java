a4e2aed8aa4d3211965f87ecc63d9bc024a02901
Choi Hyeok <114526859+Choi-Hyeok99@users.noreply.github.com>
2024-12-02 22:38:38 +0900

    [Silver IV] Title: 좋은 단어, Time: 276 ms, Memory: 22080 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -0,0 +1,40 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+/**
+ *
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+
+        int result = 0;
+
+        for (int i = 0; i < count; i++) {
+            Stack<Character> st = new Stack<>();
+            String s = br.readLine();
+            for (int j = 0; j < s.length(); j++) {
+
+                char val = s.charAt(j);
+                if (st.isEmpty()) {
+                    st.push(val);
+                } else {
+                    if (st.peek() == val) {
+                        st.pop();
+                    } else {
+                        st.push(val);
+                    }
+                }
+            }
+            if (st.empty()) {
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+}
