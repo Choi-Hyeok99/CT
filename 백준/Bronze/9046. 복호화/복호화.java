1b2b171eb6153833d606b8a831c3fe2438669b4c
Choi Hyeok <114526859+Choi-Hyeok99@users.noreply.github.com>
2025-02-06 14:39:16 +0900

    [Bronze II] Title: 복호화, Time: 112 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Bronze/9046. 복호화/복호화.java
+++ 백준/Bronze/9046. 복호화/복호화.java
@@ -1,34 +1,39 @@
 import java.io.*;
 
-
-
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
+        
+        // 입력 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = "";
-        int num = Integer.parseInt(br.readLine()); 
-        for (int h = 0; h < num; h++) {
-            str = br.readLine().replace(" ", ""); 
-            int max = 0;  
-            int count = 0; 
-
-            int[] arr = new int[26];
-            for (int i = 0; i < str.length(); i++) {
-                arr[str.charAt(i) - 'a']++;
+        
+        // 테스트 케이스 
+        int T = Integer.parseInt(br.readLine());
+        
+        while( T --> 0 ){
+            String input = br.readLine(); // 문자열 입력 받기
+            int[] arr = new int[26]; // 알파벳 저장 ( a - z )
+            
+            // 문자 개수 카운트 
+            for(char c : input.toCharArray()){
+                if(Character.isLetter(c)){
+                    arr[c - 'a' ]++; // 'a' 기준으로 배열 인덱스 계산 
+                }
             }
-            for (int j = 1; j < arr.length; j++) {
-                if (arr[max] < arr[j])
-                    max = j;
+            // 최빈 문자 찾기
+            int maxCount = 0; 
+            char maxChar = '?';
+            boolean multiple = false; 
+            
+            for(int i=0; i< 26; i++){
+                if(arr[i] > maxCount){
+                    maxCount = arr[i];
+                    maxChar = (char) (i + 'a'); 
+                    multiple = false; // 새로운 최대값으로 초기화 
+                } else if ( arr[i] == maxCount){
+                    multiple = true ; // 동일한 최빈값이 2개 이상이면 ? 출력 
+                }
             }
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[max] == arr[i])
-                    count++;
-
-            }
-
-            char ch = (char) (max + 'a');
-            System.out.println(count > 1 ? "?" : ch);
-
-        }
+            System.out.println(multiple ? "?" : maxChar);
+        }        
     }
 }
