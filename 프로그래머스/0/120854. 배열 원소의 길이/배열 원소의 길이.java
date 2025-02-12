c45a856f3e88d10cd3b46d2798ed62392d2ceabe
Choi Hyeok <114526859+Choi-Hyeok99@users.noreply.github.com>
2025-02-12 18:08:54 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.02 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        
+        for(int i=0; i< strlist.length; i++){
+            answer[i] = strlist[i].length(); // 각 문자열 길이 저장
+        }
+        return answer;
+    }
+}
